707dcecf30e75749a8c0a70f737520ae873e8370
Kamyla Estuqui Parrado <kamylaep@gmail.com>
2020-02-24 09:26:10 -0300

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,23 @@
+echo "----------> Changing Ubuntu mirror to main server"
+sudo sed -i 's|http://br.|http://|g' /etc/apt/sources.list
+
+echo "----------> Upgrading the system"
+sudo apt-get update
+sudo apt-get upgrade -y
+
+echo "----------> Installing ansible dependencies"
+sudo apt-get update
+sudo apt-get install -y software-properties-common
+
+echo "----------> Adding ansible repositository"
+sudo apt-add-repository -y ppa:ansible/ansible
+
+echo "----------> Installing ansible"
+sudo apt-get update
+sudo apt-get install -y ansible
+
+echo "----------> Downloading ansible roles"
+ansible-galaxy install -r requirements.yml -p ./external-roles --force
+
+echo "----------> Playing ansible playbooks"
+ansible-playbook playbook.yml -i inventory --ask-become-pass
